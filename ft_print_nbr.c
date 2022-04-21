296358c983a3434478972c126d1ea1fb663a9e6a
ghuertas <ghuertas@student.42madrid.com>
2022-04-21 09:18:30 +0200

    used_at_printf

---
+++ ft_print_nbr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_nbr.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/21 05:04:25 by ghuertas          #+#    #+#             */
+/*   Updated: 2022/04/21 05:06:13 by ghuertas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "printf.h"
+
+void	ft_print_nbr(int n)
+{
+	if (n == -2147483648)
+	{
+		ft_print_char('-');
+		ft_print_char('2');
+		ft_print_nbr(147483648);
+	}
+	else if (n < 0)
+	{
+		ft_print_char('-');
+		ft_print_nbr(n * -1);
+	}
+	else if (n < 10)
+		ft_print_char((char)(n + '0'));
+	else
+	{
+		ft_print_nbr(n / 10);
+		ft_print_char(n % 10 + '0');
+	}
+}
