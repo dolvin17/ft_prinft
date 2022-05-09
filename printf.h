99267d3aa4beb9ee56a8284a67056707aff4c282
ghuertas <ghuertas@student.42madrid.com>
2022-05-09 23:55:52 +0200

    updated

--- printf.h
+++ printf.h
@@ -6,7 +6,7 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 05:37:27 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/04/21 08:46:04 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/04/22 02:01:41 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
 # include <unistd.h>
 
 int		ft_printf(char const *format, ...);
-void	ft_print_char(int c);
-void	ft_print_nbr(int n);
-void	ft_putnbr_base(int n, char *base);
-void	ft_print_str(char *s);
+size_t	ft_print_char(int c);
+size_t	ft_print_nbr(int n);
+size_t	ft_putnbr_base(int n, char *base);
+size_t	ft_print_str(char *s);
 size_t	ft_strlenv2(const char *s);
 
 #endif
