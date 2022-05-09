de3aef0a35e3585270fcca4c68f79f34766a6068
ghuertas <ghuertas@student.42madrid.com>
2022-05-09 23:54:13 +0200

    updated

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,22 +6,23 @@
 /*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 04:49:13 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/04/21 05:46:07 by ghuertas         ###   ########.fr       */
+/*   Updated: 2022/04/30 20:36:03 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-void	ft_print_str(char *s)
+size_t	ft_print_str(char *s)
 {
 	int	i;
 
 	if (!s)
-		return ;
+		return (0);
 	i = 0;
 	while (s[i])
 	{
 		ft_print_char(s[i]);
 		i++;
 	}
+	return (0);
 }
