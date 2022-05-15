f487025584184650ccca5e1ce2f5144c4c22ff67
ghuertas <ghuertas@student.42madrid.com>
2022-05-15 20:32:28 +0200

    100%

--- ft_printnu_base.c
+++ ft_printnu_base.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printnu_base.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 19:25:17 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/05/15 13:59:47 by dolvin17         ###   ########.fr       */
+/*   Updated: 2022/05/15 20:25:39 by ghuertas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static size_t ft_num_len(unsigned int n)
+static size_t	ft_num_len(unsigned int n)
 {
 	size_t	len;
 
@@ -20,18 +20,19 @@ static size_t ft_num_len(unsigned int n)
 	if (n == 0)
 		len = 1;
 	while (n != 0)
-		{
-			(n /= 10);
-			len++;
-		}
-		return (len);
+	{
+		(n /= 10);
+		len++;
+	}
+	return (len);
 }
+
 int	ft_printnu_base(unsigned int n, char *base)
 {
 	unsigned int	bhex;
 
 	bhex = ft_strlenv2(base);
-		if (n == 2147483648)
+	if (n == 2147483648)
 	{
 		ft_print_char('2');
 		ft_printn_base(147483648, base);
